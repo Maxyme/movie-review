814eadb3246aeded19e038f3f0575a3103d7c919
maxyme <maxyme@outlook.com>
2018-12-12 14:25:35 -0500

    added word count and updated template

--- app.py
+++ app.py
@@ -1,27 +1,67 @@
+import operator
 import os
-from flask import Flask
+import re
+
+import nltk
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from stop_words import stops
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+# import models after db declaration due to import order
+from models import Result
 
 # sanity check
 print(os.environ['APP_SETTINGS'])
 
 
-from models import Result
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again.")
+            return render_template('index.html', errors=errors)
 
+        if r:
+            # text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+            # remove punctuation, count raw words
+            non_punct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if non_punct.match(w)]
+            raw_word_count = nltk.Counter(raw_words)
 
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = nltk.Counter(no_stop_words)
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+            # save the results
+            results = sorted(no_stop_words_count.items(), key=operator.itemgetter(1), reverse=True)
+
+            try:
+                result = Result(url=url, result_all=raw_word_count, result_no_stop_words=no_stop_words_count)
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
