2f53960fb03c52c21f4ccc05ca3b6a8b082beb39
maxyme <maxyme@outlook.com>
2018-12-15 21:53:53 -0500

    fixed settings.py to read from DATABASE_URL env variable

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,13 @@
 import os
+from urllib.parse import urlparse
 
-#SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', 5432)
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'test123')
-DB_DATABASE = os.getenv('DB_DATABASE', 'movies')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URL = os.getenv('DATABASE_URL')
+db_conn = urlparse(DATABASE_URL)
+
+DB_HOST = db_conn.hostname
+DB_PORT = db_conn.port
+DB_USER = db_conn.username
+DB_PASSWORD = db_conn.password
+
+DB_DATABASE = db_conn.path.replace('/', '')
 CSRF_ENABLED = True
